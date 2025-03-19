33412f4c7c69bad9752a906ff85374670c3d61a0
nicole0milosav <razvanpeia@gmail.com>
2025-03-19 13:51:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aplicatieandroidip/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.aplicatieandroidip;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+    public void Conectare(View v) {
+        TextView id_con = findViewById(R.id.id_conectare);
+        String id = id_con.getText().toString();
+
+        TextView parola = findViewById(R.id.parola_conectare);
+        String pass = parola.getText().toString();
+
+        if (id.equals("admin") && pass.equals("123")) {
+            ((TextView) findViewById(R.id.apa)).setText("MAIFIAAA");
+        }
+    }
+}
