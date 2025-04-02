336a4435b303de62375fb727fff38c801b012ba4
Adrian <redcharizard1@gmail.com>
2025-04-02 21:43:41 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/aplicatieandroidip/BluetoothActivity.java
@@ -0,0 +1,20 @@
+package com.example.aplicatieandroidip;
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BluetoothActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+}
